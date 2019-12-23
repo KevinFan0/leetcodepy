1ba54415e2f772c6c5f237ecb31cea84e2ebb4c2
[fandong] <[fandong@qq.com]>
2019-12-23 22:58:04 +0800

    leetcode 1

---
+++ 1_twosum.py
@@ -0,0 +1,46 @@
+# coding:utf-8
+import json
+
+
+class Solution:
+    def twoSum(self, nums: list, target: int) -> list:
+        for i in range(len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line)
+            line = next(lines)
+            target = int(line)
+
+            ret = Solution().twoSum(nums, target)
+
+            out = integerListToString(ret)
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
