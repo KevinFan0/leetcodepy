17549acce06e60c08c10618a490f6060abd143b0
[fandong] <[fandong@qq.com]>
2019-12-23 23:18:00 +0800

    update

--- 1_twosum.py
+++ 1_twosum.py
@@ -4,10 +4,12 @@ import json
 
 class Solution:
     def twoSum(self, nums: list, target: int) -> list:
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        dct = {}
+        for i, n in enumerate(nums):
+            if target - n in dct:
+                return [dct[target - n], i]
+            dct[n] = i
+
 
 def stringToIntegerList(input):
     return json.loads(input)
