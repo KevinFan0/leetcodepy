6cfbd710635a75ada9f0777ee17d4469d3b06739
fandong <fandong@ppdai.com>
2020-03-05 23:22:06 +0800

    hw test

--- aaa.py
+++ aaa.py
@@ -1,34 +1,29 @@
-# coding:utf-8
+def solve(str1):
+    data = []
+    if str1[0].isdigit():
+        data.append(0)
+    for i in range(1, len(str1)):
+        if str1[i].isdigit() ^ str1[i-1].isdigit():
+            data.append(i)
+    if str1[-1].isdigit():
+        data.append(len(str1))
+    strmax = ""
+    for i in range(len(data)):
+        if i < len(data) / 2:
+            start = data[i*2]
+            end = data[i*2 + 1]
+            if start != 0:
+                if str1[start-1] == "+" or line[start - 1] == "-":
+                    start -= 1
+            if end < len(str1) and str1[end] == "." and (i+1) * 2 < len(data) and data[(i+1)*2] == end + 1:
+                end = data[(i+1)*2 + 1]
+            if len(strmax) <= end - start:
+                strmax = str1[start: end]
+    print(data)
+    return "".join(strmax)
 
-import re
 
-while True:
-    line = str(input())
-    pat = re.compile(r"\d+")
-    s = pat.finditer(line)
-    tmp = []
-    for i in s:
-        tmp.append((i.group(), i.regs[0]))
-    max_int = max(map(int, [c[0] for c in tmp]))
-    tmp = [i for i in tmp if i[0] == str(max_int)]
-    print(tmp)
-    find = []
-    for t in tmp:
-        inx = t[1][1]
-        xiaoshu = ""
-        f = t[0]
-        if len(line) != inx and line[inx] == ".":
-            _inx = inx
-            for c in range(len(line)[inx+1:]):
-                if line[_inx+1].isdigit():
-                    _inx+=1
-                else:
-                    break
-            xiaoshu = line[inx+1: _inx+1]
-
-        if xiaoshu:
-            f = t[0] + "."+xiaoshu
-        find.append(f)
-
-    find = max(map(float, find))
-    print(find)
+if __name__ == '__main__':
+    while True:
+        line = str(input()).strip()
+        print(solve(line))
--- hw_test.py
+++ hw_test.py
@@ -3,9 +3,17 @@ def solve(line):
     data = []
     if str1[0].isdigit() or str1[0] in ["+", "-"]:
         data.append(0)
+    dot = 1
     for i in range(1, len(str1)):
-        if (str1[i].isdigit() and (str1[i-1].isdigit() or str1[i-1] == ".")) or (str1[i-1].isdigit() and str1[i] == "." and str1[i+1].isdigit()):
+        if str1[i].isdigit():
+            if str1[i-1].isdigit():
+                data.append(i)
+            if str1[i-1] == "." and dot:
+                data.append(i)
+                dot -= 1
+        if str1[i-1].isdigit() and str1[i] == "." and str1[i+1].isdigit() and dot:
             data.append(i)
+            dot -= 1
     if str1[-1].isdigit():
         data.append(len(str1))
     cur, ptr = 0, 1
@@ -15,6 +23,7 @@ def solve(line):
         else:
             cur += 1
             ptr += 1
+    print(data)
     print("".join(str1[data[cur]-1: data[ptr-1]]))
 
 
